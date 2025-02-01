393777c308dcd4e9a8cc7fa094ed4ec2f390b32a
Aalajlan6 <aalajlan6@gatech.edu>
2025-02-01 15:48:53 -0500

    Starting import for CSV file with data

--- GT_Movies_Store/models.py
+++ GT_Movies_Store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    poster = models.ImageField(upload_to='posters')
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
