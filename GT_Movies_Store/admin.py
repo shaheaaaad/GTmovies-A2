033eb448c5033e10429c0d9de639aa141a4ce90a
Rayyan Shah <rayyan.shah@gatech.edu>
2025-02-02 17:39:37 -0500

    Configure Admin functionality

--- GT_Movies_Store/admin.py
+++ GT_Movies_Store/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from .models import Movie
+
+admin.site.register(Movie)
+
+from .models import Review
+
+admin.site.register(Review)
