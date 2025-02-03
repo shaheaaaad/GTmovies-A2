8f4ba95174e81efadfa9ba43317f667335a11912
Aalajlan6 <aalajlan6@gatech.edu>
2025-02-03 11:56:48 -0500

    Added order model

--- GT_Movies_Store/models.py
+++ GT_Movies_Store/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -18,3 +18,11 @@ class Review (models.Model):
 
     def __str__(self):
         return f"Review for {self.movie.title}"
+
+class Order (models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order by {self.user.username} for {self.movie.title} on {self.order_date.strftime('%Y-%m-%d')}"
